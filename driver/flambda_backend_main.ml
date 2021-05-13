4f742d2cb9beddf777daa6af13f9daaf9a29d53f
Tristan Hume <tris.hume@gmail.com>
2021-05-13 13:50:14 -0400

    Add memtrace support (#26)

--- driver/flambda_backend_main.ml
+++ driver/flambda_backend_main.ml
@@ -1,2 +1,7 @@
 let () =
-  exit (Optmaindriver.main Sys.argv Format.err_formatter)
+  (match Sys.backend_type with
+   | Native -> Memtrace.trace_if_requested ~context:"ocamlopt" ()
+   | Bytecode | Other _ -> ());
+  let status = Optmaindriver.main Sys.argv Format.err_formatter in
+  Profile.print Format.std_formatter !Clflags.profile_columns;
+  exit status
