673aa1c9cb683ff782ba60464ca0c2176728a416
Pierre Oechsel <pierre.oechsel@gmail.com>
2023-01-10 10:57:02 +0000

    Compute global compile time

--- driver/optmaindriver.ml
+++ driver/optmaindriver.ml
@@ -54,103 +54,105 @@ let main unix argv ppf ~flambda2 =
     Format.pp_set_margin Format.std_formatter columns;
     Format.pp_set_margin Format.err_formatter columns);
   match
-    Compenv.warnings_for_discarded_params := true;
-    Compenv.set_extra_params
-      (Some Flambda_backend_args.Extra_params.read_param);
-    Compenv.readenv ppf Before_args;
-    Clflags.add_arguments __LOC__ (Arch.command_line_options @ Options.list);
-    Clflags.add_arguments __LOC__
-      ["-depend", Arg.Unit Makedepend.main_from_option,
-       "<options> Compute dependencies \
-        (use 'ocamlopt -depend -help' for details)"];
-    Clflags.Opt_flag_handler.set Flambda_backend_flags.opt_flag_handler;
-    Compenv.parse_arguments (ref argv) Compenv.anonymous "ocamlopt";
-    Compmisc.read_clflags_from_env ();
-    if !Clflags.plugin then
-      Compenv.fatal "-plugin is only supported up to OCaml 4.08.0";
-    begin try
-      Compenv.process_deferred_actions
-        (ppf,
-         Optcompile.implementation unix ~backend ~flambda2,
-         Optcompile.interface,
-         ".cmx",
-         ".cmxa");
-    with Arg.Bad msg ->
+    Profile.record_call "ocamlopt" (fun () ->
+      Compenv.warnings_for_discarded_params := true;
+      Compenv.set_extra_params
+        (Some Flambda_backend_args.Extra_params.read_param);
+      Compenv.readenv ppf Before_args;
+      Clflags.add_arguments __LOC__ (Arch.command_line_options @ Options.list);
+      Clflags.add_arguments __LOC__
+        ["-depend", Arg.Unit Makedepend.main_from_option,
+        "<options> Compute dependencies \
+          (use 'ocamlopt -depend -help' for details)"];
+      Clflags.Opt_flag_handler.set Flambda_backend_flags.opt_flag_handler;
+      Compenv.parse_arguments (ref argv) Compenv.anonymous "ocamlopt";
+      Compmisc.read_clflags_from_env ();
+      if !Clflags.plugin then
+        Compenv.fatal "-plugin is only supported up to OCaml 4.08.0";
+      begin try
+        Compenv.process_deferred_actions
+          (ppf,
+          Optcompile.implementation unix ~backend ~flambda2,
+          Optcompile.interface,
+          ".cmx",
+          ".cmxa");
+      with Arg.Bad msg ->
+        begin
+          prerr_endline msg;
+          Clflags.print_arguments usage;
+          exit 2
+        end
+      end;
+      Compenv.readenv ppf Before_link;
+      if
+        List.length (List.filter (fun x -> !x)
+                      [make_package; make_archive; shared;
+                        Compenv.stop_early; output_c_object]) > 1
+      then
       begin
-        prerr_endline msg;
-        Clflags.print_arguments usage;
-        exit 2
+        let module P = Clflags.Compiler_pass in
+        match !stop_after with
+        | None ->
+            Compenv.fatal "Please specify at most one of -pack, -a, -shared, -c, \
+                          -output-obj";
+        | Some ((P.Parsing | P.Typing | P.Scheduling
+                | P.Simplify_cfg | P.Emit | P.Selection) as p) ->
+          assert (P.is_compilation_pass p);
+          Printf.ksprintf Compenv.fatal
+            "Options -i and -stop-after (%s) \
+            are  incompatible with -pack, -a, -shared, -output-obj"
+            (String.concat "|"
+              (P.available_pass_names ~filter:(fun _ -> true) ~native:true))
+      end;
+      if !make_archive then begin
+        Compmisc.init_path ();
+        let target = Compenv.extract_output !output_name in
+        Asmlibrarian.create_archive
+          (Compenv.get_objfiles ~with_ocamlparam:false) target;
+        Warnings.check_fatal ();
       end
-    end;
-    Compenv.readenv ppf Before_link;
-    if
-      List.length (List.filter (fun x -> !x)
-                     [make_package; make_archive; shared;
-                      Compenv.stop_early; output_c_object]) > 1
-    then
-    begin
-      let module P = Clflags.Compiler_pass in
-      match !stop_after with
-      | None ->
-          Compenv.fatal "Please specify at most one of -pack, -a, -shared, -c, \
-                         -output-obj";
-      | Some ((P.Parsing | P.Typing | P.Scheduling
-              | P.Simplify_cfg | P.Emit | P.Selection) as p) ->
-        assert (P.is_compilation_pass p);
-        Printf.ksprintf Compenv.fatal
-          "Options -i and -stop-after (%s) \
-           are  incompatible with -pack, -a, -shared, -output-obj"
-          (String.concat "|"
-             (P.available_pass_names ~filter:(fun _ -> true) ~native:true))
-    end;
-    if !make_archive then begin
-      Compmisc.init_path ();
-      let target = Compenv.extract_output !output_name in
-      Asmlibrarian.create_archive
-        (Compenv.get_objfiles ~with_ocamlparam:false) target;
-      Warnings.check_fatal ();
-    end
-    else if !make_package then begin
-      Compmisc.init_path ();
-      let target = Compenv.extract_output !output_name in
-      Compmisc.with_ppf_dump ~file_prefix:target (fun ppf_dump ->
-        Asmpackager.package_files unix
-          ~ppf_dump (Compmisc.initial_env ())
-          (Compenv.get_objfiles ~with_ocamlparam:false) target ~backend
-          ~flambda2);
-      Warnings.check_fatal ();
-    end
-    else if !shared then begin
-      Compmisc.init_path ();
-      let target = Compenv.extract_output !output_name in
-      Compmisc.with_ppf_dump ~file_prefix:target (fun ppf_dump ->
-        Asmlink.link_shared ~ppf_dump
-          (Compenv.get_objfiles ~with_ocamlparam:false) target);
-      Warnings.check_fatal ();
-    end
-    else if not !Compenv.stop_early && !objfiles <> [] then begin
-      let target =
-        if !output_c_object then
-          let s = Compenv.extract_output !output_name in
-          if (Filename.check_suffix s Config.ext_obj
-            || Filename.check_suffix s Config.ext_dll)
-          then s
+      else if !make_package then begin
+        Compmisc.init_path ();
+        let target = Compenv.extract_output !output_name in
+        Compmisc.with_ppf_dump ~file_prefix:target (fun ppf_dump ->
+          Asmpackager.package_files unix
+            ~ppf_dump (Compmisc.initial_env ())
+            (Compenv.get_objfiles ~with_ocamlparam:false) target ~backend
+            ~flambda2);
+        Warnings.check_fatal ();
+      end
+      else if !shared then begin
+        Compmisc.init_path ();
+        let target = Compenv.extract_output !output_name in
+        Compmisc.with_ppf_dump ~file_prefix:target (fun ppf_dump ->
+          Asmlink.link_shared ~ppf_dump
+            (Compenv.get_objfiles ~with_ocamlparam:false) target);
+        Warnings.check_fatal ();
+      end
+      else if not !Compenv.stop_early && !objfiles <> [] then begin
+        let target =
+          if !output_c_object then
+            let s = Compenv.extract_output !output_name in
+            if (Filename.check_suffix s Config.ext_obj
+              || Filename.check_suffix s Config.ext_dll)
+            then s
+            else
+              Compenv.fatal
+                (Printf.sprintf
+                  "The extension of the output file must be %s or %s"
+                  Config.ext_obj Config.ext_dll
+                )
           else
-            Compenv.fatal
-              (Printf.sprintf
-                 "The extension of the output file must be %s or %s"
-                 Config.ext_obj Config.ext_dll
-              )
-        else
-          Compenv.default_output !output_name
-      in
-      Compmisc.init_path ();
-      Compmisc.with_ppf_dump ~file_prefix:target (fun ppf_dump ->
-          let objs = Compenv.get_objfiles ~with_ocamlparam:true in
-          Asmlink.link unix
-            ~ppf_dump objs target);
-      Warnings.check_fatal ();
-    end;
+            Compenv.default_output !output_name
+        in
+        Compmisc.init_path ();
+        Compmisc.with_ppf_dump ~file_prefix:target (fun ppf_dump ->
+            let objs = Compenv.get_objfiles ~with_ocamlparam:true in
+            Asmlink.link unix
+              ~ppf_dump objs target);
+        Warnings.check_fatal ();
+      end;
+    )
   with
   | exception (Compenv.Exit_with_status n) ->
     n
